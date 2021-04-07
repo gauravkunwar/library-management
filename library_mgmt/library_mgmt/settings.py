3473f206bd924deba5ac0394ca2166a87608dfa7
Gaurav Kunwar <gauravkunwar@aol.com>
2021-04-07 17:45:23 +1000

    err fixes

--- library_mgmt/library_mgmt/settings.py
+++ library_mgmt/library_mgmt/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from secrets import *
+# imported from 'secrets.py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
